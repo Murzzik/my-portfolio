5cc0a42c8fb83f32b4e35baed8ab6666827629a1
Murzzik <mcalexstar@gmail.com>
2022-04-17 20:04:41 +0300

    update animation
    Add text animation

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -9,7 +9,7 @@ const Main = () => {
                 <div className={s.Greetings}>
                     <span>Hi There!</span>
                     <h1>I am <span className={s.authorName}>Alexander Storozhuk</span></h1>
-                    <p>Frontend Developer</p>
+                    <p className={s.typewriter}>&lt;Frontend Developer/&gt;</p>
                 </div>
                 <div className={s.GreetingsPhoto}>Some cool photo may be <span>here</span></div>
             </div>
